ff16461f3f6d2d8a6bc5f10a083dcaf162c99eaf
Daniel Muckelbauer <dadamuck@hotmail.com>
2021-09-17 21:36:42 +0200

    TestView and BindableBase without testing

---
+++ EasyRooms/ViewModel/BindableBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EasyRooms.ViewModel;
+
+public class BindableBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+
+    protected virtual void SetProperty<T>(ref T member, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Equals(member, value))
+        {
+            return;
+        }
+        member = value;
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName) 
+        => PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+}
