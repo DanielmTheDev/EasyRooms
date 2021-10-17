086da6b93dc9e78e6c4f723e305f207ff0f7801a
Daniel Muckelbauer <Daniel.Muckelbauer@L-P-A.com>
2021-10-17 13:20:49 +0200

    Add FreeRoomFinderTest

--- EasyRooms.Tests/UnitTests/FreeRoomFinderTests.cs
+++ EasyRooms.Tests/UnitTests/FreeRoomFinderTests.cs
@@ -16,18 +16,28 @@ public class FreeRoomFinderTests
 {
     private readonly FreeRoomFinder _freeRoomFinder;
 
-    public FreeRoomFinderTests() 
+    public FreeRoomFinderTests()
         => _freeRoomFinder = new FreeRoomFinder();
 
     [Fact]
     public void Finds_First_Free_Room()
     {
-        var rooms = CreateRooms();
+        var rooms = CreateRooms().ToList();
+        var freeRoom = _freeRoomFinder.CalculateOccupationCreationData("08:00", "120", 0, rooms);
+        freeRoom.Should().Be(rooms.Last());
     }
 
     private IEnumerable<Room> CreateRooms()
     {
-        var room1 = new Room("room1", 0)
-            .AddOccupation(new Occupation(new )
+        var row1 = CreateDefaultRow();
+        var room1 = new Room("room1", 0).AddOccupation(new Occupation(row1, new TimeSpan(8, 0, 0),
+            new TimeSpan(10, 0, 0)));
+        var room2 = new Room("room2", 1).AddOccupation(new Occupation(row1, new TimeSpan(8, 0, 0),
+            new TimeSpan(10, 0, 0)));
+        var room3 = new Room("room3", 2);
+        return new List<Room> { room1, room2, room3 };
     }
+
+    private Row CreateDefaultRow()
+        => new Row("", "", "sh", "lo", "patient", "therapist");
 }
