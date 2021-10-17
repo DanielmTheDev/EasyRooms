b6e74eb23d8faba2d369ef2495ee66bd1c3eae0b
Daniel Muckelbauer <Daniel.Muckelbauer@L-P-A.com>
2021-10-17 11:34:29 +0200

    Tests WIP

---
+++ EasyRooms.Tests/UnitTests/FreeRoomFinderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using EasyRooms.Model.Rooms;
+using EasyRooms.Model.Rooms.Models;
+using EasyRooms.Model.Rows.Models;
+using EasyRooms.Model.Therapy;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace EasyRooms.Tests.UnitTests;
+
+public class FreeRoomFinderTests
+{
+    private readonly FreeRoomFinder _freeRoomFinder;
+
+    public FreeRoomFinderTests() 
+        => _freeRoomFinder = new FreeRoomFinder();
+
+    [Fact]
+    public void Finds_First_Free_Room()
+    {
+        var rooms = CreateRooms();
+    }
+
+    private IEnumerable<Room> CreateRooms()
+    {
+        var room1 = new Room("room1", 0)
+            .AddOccupation(new Occupation(new )
+    }
+}
