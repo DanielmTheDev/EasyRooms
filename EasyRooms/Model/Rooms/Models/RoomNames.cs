ad60c90638e0fe5ed70fb0ada6a264157443dadd
Daniel Muckelbauer <dadamuck@hotmail.com>
2021-11-01 22:28:43 +0100

    Add default values for rooms

--- EasyRooms/Model/Rooms/Models/RoomNames.cs
+++ EasyRooms/Model/Rooms/Models/RoomNames.cs
@@ -8,13 +8,23 @@ namespace EasyRooms.Model.Rooms.Models
     {
         public string RoomsString { get; set; }
         public string PartnerRoomString { get; set; }
+        public string RoomsForSpecificMassages { get; set; }
+        public string MassagesForSpecificRooms { get; set; }
+        
         public IEnumerable<string> AllRoomsAsList => RoomsString.Split('\n');
         public IEnumerable<string> PartnerRoomsRoomsAsList => PartnerRoomString.Split('\n');
 
-        public RoomNames(string roomsString = "Raum1\nRaum2\nRaum3\nRaum4\nRaum5\nRaum6\nRaum7\nRaum8\n", string partnerRoomString = "Raum1\nRaum2")
+        public RoomNames(
+            string roomsString = "Raum1\nRaum2\nRaum3\nRaum4\nRaum5\nRaum6\nRaum7\nRaum8\n", 
+            string partnerRoomString = "Raum1\nRaum2",
+            string roomsForSpecificMassages = "Raum3\nRaum4\n",
+            string massagesForSpecificRooms = "RÜCK40\nFUSS\n")
         {
             RoomsString = roomsString;
             PartnerRoomString = partnerRoomString;
+            RoomsForSpecificMassages = roomsForSpecificMassages;
+            MassagesForSpecificRooms = massagesForSpecificRooms;
+            GuardPartnerRooms();
         }
 
         private void GuardPartnerRooms()
