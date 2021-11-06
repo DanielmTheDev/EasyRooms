511b312be772d6f7a716ab86e2878d6c9de553d0
Daniel Muckelbauer <dadamuck@hotmail.com>
2021-11-06 21:10:24 +0100

    Create roomspecfic test

--- EasyRooms.Tests/UnitTests/RoomListCreatorTests.cs
+++ EasyRooms.Tests/UnitTests/RoomListCreatorTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using EasyRooms.Model.Rooms;
 using EasyRooms.Model.Rooms.Models;
@@ -20,14 +21,41 @@ namespace EasyRooms.Tests.UnitTests
             const string roomString = "room1\nroom2\nroom3";
             const string partnerString = "room2\nroom3";
             var roomNames = new RoomNames(roomString, partnerString);
-            var expectedRooms = CreateExpectedRooms().ToList();
+            var expectedRooms = CreateExpectedPartnerRooms();
+            
+            var rooms = _roomListCreator.CreateRooms(roomNames);
+            
+            rooms.Should().BeEquivalentTo(expectedRooms, config => config.ComparingByMembers<Room>());
+        }
+        
+        [Fact]
+        public void Creates_Room_With_MassageSpecificRoom_Properties()
+        {
+            const string roomString = "room1\nroom2\nroom3";
+            const string roomSpecificRooms = "room2\nroom3";
+            var roomNames = new RoomNames(roomString, "room1", roomSpecificRooms);
+            var expectedRooms = CreateExpectedMassageSpecificRooms();
             
             var rooms = _roomListCreator.CreateRooms(roomNames);
             
             rooms.Should().BeEquivalentTo(expectedRooms, config => config.ComparingByMembers<Room>());
         }
 
-        private static IEnumerable<Room> CreateExpectedRooms()
+        private IEnumerable<Room> CreateExpectedMassageSpecificRooms()
+        {
+            var expectedRooms = new List<Room>
+            {
+                new("room1", 0),
+                new("room2", 1),
+                new("room3", 2),
+            };
+            expectedRooms[0].IsPartnerRoom = true;
+            expectedRooms[1].IsMassageSpecificRoom = true;
+            expectedRooms[2].IsMassageSpecificRoom = true;
+            return expectedRooms;
+        }
+
+        private static IEnumerable<Room> CreateExpectedPartnerRooms()
         {
             var expectedRooms = new List<Room>
             {
