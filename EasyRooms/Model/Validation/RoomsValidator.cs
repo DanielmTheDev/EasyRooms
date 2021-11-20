3018675254f10cc3677f44336d748d698515854c
Daniel Muckelbauer <dadamuck@hotmail.com>
2021-11-20 18:13:22 +0100

    Refactor rooms validator

--- EasyRooms/Model/Validation/RoomsValidator.cs
+++ EasyRooms/Model/Validation/RoomsValidator.cs
@@ -9,25 +9,26 @@ namespace EasyRooms.Model.Validation
     {
         public bool IsValid(IEnumerable<Room> rooms, RoomNames roomNames)
         {
-            return rooms.All(room => room.Occupations.All(occupation =>
+            return rooms.All(room =>
             {
                 if (OccupationsOverlap(room))
                     return false;
-                if (occupation.TherapyShort == CommonConstants.PartnerString)
-                    return ValidatePartnerMassages(occupation, roomNames, room);
-                else
+                return room.Occupations.All(occupation =>
+                {
+                    if (occupation.TherapyShort == CommonConstants.PartnerString)
+                        return ValidatePartnerMassages(roomNames, room);
                     return true;
-            }));
+                });
+            });
         }
 
         //continue here, doesn't work
         private static bool OccupationsOverlap(Room room)
             => room.Occupations.All(occupation1 => !room.Occupations
-                .Any(occupation2 =>
-                    occupation1.StartTime < occupation2.EndTime
-                    && occupation2.StartTime < occupation1.EndTime));
+                .Any(occupation2 => occupation1.StartTime < occupation2.EndTime
+                                    && occupation2.StartTime < occupation1.EndTime));
 
-        private static bool ValidatePartnerMassages(Occupation occupation, RoomNames roomNames, Room room)
+        private static bool ValidatePartnerMassages(RoomNames roomNames, Room room)
             => roomNames.PartnerRoomsRoomsAsList.Contains(room.Name);
     }
 }
