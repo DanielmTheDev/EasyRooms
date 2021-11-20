fcbc2e90dba603b56472e4a8a3cc9aa8786d66a1
Daniel Muckelbauer <dadamuck@hotmail.com>
2021-11-20 18:53:31 +0100

    Fix room validator tests

--- EasyRooms/Model/Rooms/Models/RoomNames.cs
+++ EasyRooms/Model/Rooms/Models/RoomNames.cs
@@ -18,7 +18,7 @@ namespace EasyRooms.Model.Rooms.Models
 
 
         public RoomNames(
-            string roomsString = "Raum1\nRaum2\nRaum3\nRaum4\nRaum5\nRaum6\nRaum7\nRaum8\n", 
+            string roomsString = "Raum1\nRaum2\nRaum3\nRaum4\nRaum5\nRaum6\nRaum7\nRaum8", 
             string partnerRoomString = "Raum1\nRaum2",
             string roomsForSpecificMassages = "Raum3\nRaum4\n",
             string massagesForSpecificRooms = "RÜCK40\nFUSS\n")
