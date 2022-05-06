a76577867a31374beb5e70e097a1b46eab4c48c3
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-05-06 17:19:08 +0200

    Write to relative path

--- EasyRooms/Model/Pdf/Implementations/PdfWriter.cs
+++ EasyRooms/Model/Pdf/Implementations/PdfWriter.cs
@@ -20,9 +20,10 @@ public class PdfWriter : IPdfWriter
 
     private static void CreateAndOpenFile(PdfData pdf)
     {
-        var path = $@"C:\Users\dadam\Downloads\{pdf.Name}.pdf";
+        var path = $@".\Pläne\{pdf.Name}.pdf";
+        Directory.CreateDirectory(Path.GetDirectoryName(path)!);
         File.WriteAllBytes(path, pdf.Builder.Build());
-        OpenPdf(path);
+        // OpenPdf(path);
     }
 
     private static void OpenPdf(string filePath)
