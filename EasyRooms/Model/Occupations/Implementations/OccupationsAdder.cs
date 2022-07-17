cc0f243533c9a162a1e590992e72deaf51dbb845
DanielMuckelbauer <dadamuck@hotmail.com>
2022-07-17 11:20:12 +0200

    Fix endtime buffer

--- EasyRooms/Model/Occupations/Implementations/OccupationsAdder.cs
+++ EasyRooms/Model/Occupations/Implementations/OccupationsAdder.cs
@@ -1,3 +1,4 @@
+using EasyRooms.Model.CommonExtensions;
 using EasyRooms.Model.Rooms.Interfaces;
 
 namespace EasyRooms.Model.Occupations.Implementations;
@@ -11,19 +12,13 @@ public class OccupationsAdder : IOccupationsAdder
 
     public void AddToFreeRoom(IEnumerable<Room> rooms, int bufferInMinutes, params Row[] rows)
     {
-        var freeRoom = _freeRoomFinder.FindFreeRoom(rows.First().StartTimeAsTimeSpan, rows.First().EndTimeAsTimeSpan, bufferInMinutes, rooms);
-        foreach (var row in rows)
-        {
-            freeRoom.AddOccupation(new(row, row.StartTimeAsTimeSpan, row.EndTimeAsTimeSpan));
-        }
+        var freeRoom = _freeRoomFinder.FindFreeRoom(rows.First().StartTimeAsTimeSpan, rows.Last().EndTimeAsTimeSpan, bufferInMinutes, rooms);
+        rows.ForEach(row => freeRoom.AddOccupation(new(row, row.StartTimeAsTimeSpan, row.EndTimeAsTimeSpan)));
     }
 
     public void AddToSpecificRoom(IEnumerable<Room> rooms, string roomName, params Row[] rows)
     {
         var specificRoom = rooms.Single(room => string.Equals(room.Name, roomName, StringComparison.OrdinalIgnoreCase));
-        foreach (var row in rows)
-        {
-            specificRoom.AddOccupation(new(row, row.StartTimeAsTimeSpan, row.EndTimeAsTimeSpan));
-        }
+        rows.ForEach(row => specificRoom.AddOccupation(new(row, row.StartTimeAsTimeSpan, row.EndTimeAsTimeSpan)));
     }
 }
